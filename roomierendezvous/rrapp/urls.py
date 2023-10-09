5ac6b9dabc8258c66f7a47c9cf33c226e72deb95
Zhiyuan-Wei <120235952+Zhiyuan-Wei@users.noreply.github.com>
2023-10-08 22:56:32 -0400

    Update urls.py

--- roomierendezvous/rrapp/urls.py
+++ roomierendezvous/rrapp/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path('<int:user_id>/listings/', views.ListingIndexView.as_view(), name='my_listings'),
     # ex: /rrapp/5/listings/new
     path('<int:user_id>/listings/new', views.ListingNewView.as_view(), name='listing_new'),
+    # ex: /rrapp/5/delete/1
+    path('<int:user_id>/delete/<int:pk>', views.listing_delete, name='listing_delete'),
 
 ]
