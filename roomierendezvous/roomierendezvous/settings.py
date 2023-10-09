265edfd95713f66a5f60abbb139071b3eebae73a
himanshu1196 <11.himanshu.96@gmail.com>
2023-10-09 17:35:22 -0400

    Updates to fix eb deploy

--- roomierendezvous/roomierendezvous/settings.py
+++ roomierendezvous/roomierendezvous/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +78,26 @@ WSGI_APPLICATION = 'roomierendezvous.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-images',
+        'USER': 'django-images',
+        'PASSWORD': 'complexpassword123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 
 
 # Password validation
