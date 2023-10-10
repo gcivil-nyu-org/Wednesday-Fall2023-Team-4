19d9b61f89d2a1119085120e909bdf4bed92c30c
SycamoreSt <zw3743@nyu.edu>
2023-10-10 19:09:03 -0400

    Update settings.py

--- roomierendezvous/roomierendezvous/settings.py
+++ roomierendezvous/roomierendezvous/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'roomierendezvous.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'NAME': 'django-images',
+        'USER': 'django-images',
+        'PASSWORD': 'complexpassword123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
