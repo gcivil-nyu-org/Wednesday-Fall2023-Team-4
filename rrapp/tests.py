bf89a819795833a93507b77bfa4115dbfbce8c28
himanshu1196 <11.himanshu.96@gmail.com>
2023-10-17 13:31:41 -0400

    Enabling travis ci and coveralls

--- rrapp/tests.py
+++ rrapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from rrapp.models import Listing
 
 # Create your tests here.
+
+class ListingTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_animals_can_speak(self):
+        """Question should be returned with the query"""
+        return True
