21cc2654df1f1a9968856a5b76dfcfbd79727082
Zhiyuan-Wei <120235952+Zhiyuan-Wei@users.noreply.github.com>
2023-10-31 19:57:40 -0400

    Update views.py

--- rrapp/views.py
+++ rrapp/views.py
@@ -1,12 +1,11 @@
 from typing import Any
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-from django import forms
 
 from psycopg2.extras import NumericRange
 
