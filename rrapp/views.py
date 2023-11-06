a3fb79f532fdcbc81811fcfb3dd3d4c1d53e1c38
Zeno Wang <zenowang1999@gmail.com>
2023-11-05 21:31:41 -0500

    reconstruct the css, add profile page

--- rrapp/views.py
+++ rrapp/views.py
@@ -157,7 +157,9 @@ class ListingIndexView(generic.ListView):
 
     def get_context_data(self, **kwargs: Any):
         context_data = super().get_context_data(**kwargs)
-        context_data["user_id"] = self.kwargs["user_id"]
+        user_id = self.kwargs["user_id"]
+        context_data["user_id"] = user_id
+        context_data["user"] = User.objects.get(id=user_id)
         return context_data
 
 
@@ -341,7 +343,32 @@ class ListingNewView(generic.CreateView):
             reverse('rrapp:my_listings', args=(kwargs["user_id"],))
         )
 
-
+@method_decorator(login_required, name='dispatch')
+class ProfileView(generic.UpdateView):
+    model = User
+    template_name = "rrapp/profile.html"
+    fields = [
+        'username',
+        'birth_date',
+        'first_name',
+        'last_name',
+        'bio',
+        'smokes',
+        'pets',
+        'food_group',
+        'phone_number',
+    ]
+    success_url = 'rrapp:rentee_listings'
+    def get_context_data(self, **kwargs: Any):
+        context_data = super().get_context_data(**kwargs)
+        context_data["user_id"] = self.kwargs["pk"]
+        context_data["user"] = User.objects.get(id=self.kwargs["pk"])
+        return context_data
+    
+    def get_success_url(self):
+        user_id = self.kwargs['pk']
+        return reverse('rrapp:rentee_listings', args=(user_id,))
+    
 @login_required(login_url='login')
 def listing_delete(request, user_id, pk):
     # TODO:add the check  if request.user.is_authenticated():
