cfd0a40850d73ca3a667bf7a92cdd3ecc6ae9eff
Devarshi Chatterjee <dc5212@nyu.edu>
2023-11-09 09:50:44 -0500

    CHanges 2

--- rrapp/tests.py
+++ rrapp/tests.py
@@ -143,3 +143,173 @@ class ListingResultsViewTest(TestCase):
             title="Test Listing",
             monthly_rent=1000,
         )
+
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import Listing, Rentee, SavedListing
+
+User = get_user_model()
+
+
+class ViewsTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.client = Client()
+        cls.user = User.objects.create_user(
+            email="test@example.com", password="testpassword"
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        # Clean up any test-specific data
+        super().tearDownClass()
+
+
+class HomeViewTest(ViewsTestCase):
+    def test_home_view_redirect_authenticated_user(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("rrapp:home"))
+        self.assertRedirects(
+            response, reverse("rrapp:my_listings", args=(self.user.id,))
+        )
+
+
+class LoginViewTest(ViewsTestCase):
+    def test_login_view_get(self):
+        response = self.client.get(reverse("rrapp:login"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "rrapp/login_register.html")
+
+    def test_login_view_post_valid_credentials(self):
+        response = self.client.post(
+            reverse("rrapp:login"),
+            {"email": "test@example.com", "password": "testpassword"},
+        )
+        self.assertRedirects(
+            response, reverse("rrapp:rentee_listings", args=(self.user.id,))
+        )
+
+    def test_login_view_post_invalid_credentials(self):
+        response = self.client.post(
+            reverse("rrapp:login"),
+            {"email": "test@example.com", "password": "wrongpassword"},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "rrapp/login_register.html")
+
+
+class LogoutViewTest(ViewsTestCase):
+    def test_logout_view(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("rrapp:logout"))
+        self.assertRedirects(response, reverse("rrapp:home"))
+
+
+class RegisterViewTest(ViewsTestCase):
+    def test_register_view_get(self):
+        response = self.client.get(reverse("rrapp:register"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "rrapp/login_register.html")
+
+    def test_register_view_post_invalid_credentials(self):
+        response = self.client.post(
+            reverse("rrapp:register"),
+            {
+                "email": "test@example.com",
+                "password1": "testpassword",
+                "password2": "wrongpassword",
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "rrapp/login_register.html")
+
+
+class ListingIndexViewTest(ViewsTestCase):
+    def test_listing_index_view_authenticated_user(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("rrapp:my_listings", args=(self.user.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "rrapp/listing_index.html")
+
+
+class ListingDetailViewTest(ViewsTestCase):
+    def test_listing_detail_view_authenticated_user(self):
+        self.client.force_login(self.user)
+        listing = Listing.objects.create(
+            user=self.user, title="Test Listing", monthly_rent=1000
+        )
+        response = self.client.get(
+            reverse(
+                "rrapp:listing_detail",
+                args=(
+                    self.user.id,
+                    listing.id,
+                ),
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "rrapp/listing_detail.html")
+
+
+class ListingDetailRenteeViewTest(ViewsTestCase):
+    def test_listing_detail_rentee_view_authenticated_user(self):
+        self.client.force_login(self.user)
+        rentee = Rentee.objects.create(user=self.user)
+        listing = Listing.objects.create(
+            user=User.objects.create_user(
+                username="testuser2", password="testpass2", email="testuser@example.com"
+            ),
+            title="Test Listing",
+            monthly_rent=1000,
+        )
+        response = self.client.get(
+            reverse(
+                "rrapp:rentee_listing_detail",
+                args=(
+                    self.user.id,
+                    listing.id,
+                ),
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "rrapp/listing_detail_rentee.html")
+
+    def test_listing_detail_rentee_view_save_listing(self):
+        self.client.force_login(self.user)
+        rentee = Rentee.objects.create(user=self.user)
+        listing = Listing.objects.create(
+            user=User.objects.create_user(
+                username="testuser2", password="testpass2", email="testuser@example.com"
+            ),
+            title="Test Listing",
+            monthly_rent=1000,
+        )
+        response = self.client.post(
+            reverse(
+                "rrapp:rentee_listing_detail",
+                args=(
+                    self.user.id,
+                    listing.id,
+                ),
+            ),
+            {"shortlist": "true"},
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            SavedListing.objects.filter(
+                rentee_id__user=self.user.id, saved_listings=listing.id
+            ).exists()
+        )
+
+
+class ListingResultsViewTest(ViewsTestCase):
+    def test_listing_results_view_authenticated_user(self):
+        self.client.force_login(self.user)
+        response = self.client.get(
+            reverse("rrapp:rentee_listings", args=(self.user.id,))
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "rrapp/listing_results.html")
