498f42c8d9776dbba765fac7d8f10b5cedfd64bb
himanshu1196 <11.himanshu.96@gmail.com>
2023-11-10 03:24:44 -0500

    HTTP based chat view and urls; code cleanup

--- chat/models.py
+++ chat/models.py
@@ -2,17 +2,6 @@ from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.utils import timezone
 
-
-class Message(models.Model):
-    username = models.CharField(max_length=255)
-    room = models.CharField(max_length=255)
-    content = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ('date_added',)
-
-
 class DirectMessage(models.Model):
     sender = models.CharField(max_length=255)
     receiver = models.CharField(max_length=255)
--- chat/views.py
+++ chat/views.py
@@ -4,24 +4,14 @@ from django.views import generic
 
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.core.exceptions import PermissionDenied
 
 from rrapp.models import User
-from .models import Message, DirectMessage, DirectMessagePermission, Permission
+from .models import DirectMessage, DirectMessagePermission, Permission
 
 
 def index(request):
     return render(request, 'chat/index.html')
-
-
-def room(request, room_name):
-    username = request.GET.get('username', 'Anonymous')
-    messages = Message.objects.filter(room=room_name)[0:25]
-
-    return render(
-        request,
-        'chat/room.html',
-        {'room_name': room_name, 'username': username, 'messages': messages},
-    )
 
 
 def conversation(request, receiverUsername):
@@ -90,6 +80,15 @@ class ConversationHomeView(generic.View):
             )
         except DirectMessagePermission.DoesNotExist:
             pending_connections = []
+        
+        all_pending_connection_usernamesids = []
+        for p in pending_connections:
+            all_pending_connection_usernamesids.append(
+                {
+                    'id': User.objects.get(username=p.sender).id,
+                    'username': p.sender,
+                }
+            )
 
         try:
             active_connections = list(
@@ -101,12 +100,9 @@ class ConversationHomeView(generic.View):
         except DirectMessagePermission.DoesNotExist:
             active_connections = []
 
-        all_active_connection_usernames = []
         all_active_connection_usernamesids = []
         for p in active_connections:
             if p.sender == cur_username:
-                all_active_connection_usernames.append(p.receiver)
-
                 all_active_connection_usernamesids.append(
                     {
                         'id': User.objects.get(username=p.receiver).id,
@@ -114,7 +110,6 @@ class ConversationHomeView(generic.View):
                     }
                 )
             else:
-                all_active_connection_usernames.append(p.sender)
                 all_active_connection_usernamesids.append(
                     {'id': User.objects.get(username=p.sender).id, 'username': p.sender}
                 )
@@ -128,17 +123,24 @@ class ConversationHomeView(generic.View):
             )
         except DirectMessagePermission.DoesNotExist:
             requested_connections = []
+        
+        all_requested_connection_usernamesids = []
+        for p in requested_connections:
+            all_requested_connection_usernamesids.append(
+                {
+                    'id': User.objects.get(username=p.receiver).id,
+                    'username': p.receiver,
+                }
+            )
 
         return render(
             request,
             'chat/conversation_home.html',
             {
                 'cur_username': cur_username,
-                'pending_connections': pending_connections,
-                # TODO : change to usernames after aws daphne integration
-                # 'active_connections': all_active_connection_usernames,
+                'pending_connections': all_pending_connection_usernamesids,
                 'active_connections': all_active_connection_usernamesids,
-                'requested_connections': requested_connections,
+                'requested_connections': all_requested_connection_usernamesids,
             },
         )
 
@@ -159,3 +161,74 @@ class ConversationHomeView(generic.View):
             pass
 
         return HttpResponseRedirect(reverse("chat:conversation_home"))
+
+
+class ConversationView(generic.View):
+    def dispatch(self, request, *args, **kwargs):
+        # will redirect to the home page if a user tries to
+        # access the register page while logged in
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("rrapp:login"))
+        # else process dispatch as it otherwise normally would
+        return super(ConversationView, self).dispatch(request, *args, **kwargs)
+    
+    def canDmReceiver(self, senderUsername, receiverUsername):
+        try:
+            permissions = list(
+                DirectMessagePermission.objects.filter(
+                    (Q(sender__exact=senderUsername) & Q(receiver__exact=receiverUsername)) | 
+                    (Q(sender__exact=receiverUsername) & Q(receiver__exact=senderUsername))
+                )
+            )
+        except DirectMessagePermission.DoesNotExist:
+            # permission does not exist
+            return False
+
+        if len(permissions) == 0:
+            return False
+        
+        p = permissions[0]
+        if p.permission != Permission.ALLOWED:
+            return False
+        
+        return True
+
+    def get(self, request, receiverUsername, *args, **kwargs):
+        senderUsername = request.user.username
+        room_name = '_'.join(sorted([senderUsername, receiverUsername]))
+
+        if not self.canDmReceiver(senderUsername, receiverUsername):
+            print(senderUsername, receiverUsername)
+            raise PermissionDenied('The receiver needs to accept your request before your can message them.')
+        
+        # TODO paginate
+        messages = DirectMessage.objects.filter(room=room_name)
+
+        # ALLOWED permission exists in DB
+        # render the page and handle messages
+        receiverUser = User.objects.get(username=receiverUsername)
+        receiverUsernameId = {"username": receiverUsername, "id":receiverUser.id}
+        return render(
+            request,
+            'chat/conversation_http.html',
+            {
+                'room_name': room_name,
+                'sender': senderUsername,
+                'receiver': receiverUsernameId,
+                'messages': messages,
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+        senderUsername = request.user.username
+        receiverUsername = kwargs["receiverUsername"]
+
+        if not self.canDmReceiver(senderUsername, receiverUsername):
+            raise PermissionDenied('The receiver needs to accept your request before your can send messages to them.')
+        
+        if 'chat-message-input' in request.POST:
+            room_name = '_'.join(sorted([senderUsername, receiverUsername]))
+            print('saving ', request.POST)
+            DirectMessage.objects.create(sender=senderUsername, receiver=receiverUsername, room=room_name, content=request.POST['chat-message-input'])
+
+        return HttpResponseRedirect(reverse("chat:conversation", kwargs={"receiverUsername": receiverUsername}))
