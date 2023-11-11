8923dd4e3bf09cac8bda8b74c3122966218bc068
Zhiyuan Wei <zw3475@nyu.edu>
2023-11-10 23:38:54 -0500

    comment

--- rrapp/views.py
+++ rrapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render
 from typing import Any, List
 from django.db.models import Q
 
