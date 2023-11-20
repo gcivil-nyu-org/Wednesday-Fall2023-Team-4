328c7fb10253866aafd19ee36075f1f067e46c5c
Zhiyuan Wei <zw3475@nyu.edu>
2023-11-20 17:27:41 -0500

    commit

--- rrapp/views.py
+++ rrapp/views.py
@@ -679,7 +679,9 @@ def listing_delete(request, user_id, pk):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('rrapp:my_listings', args=(user_id,)))
-    return render(request, 'rrapp/confirm_delete_listing.html', {"user_id": user_id, "pk": pk})
+    return render(
+        request, 'rrapp/confirm_delete_listing.html', {"user_id": user_id, "pk": pk}
+    )
 
 
 @login_required(login_url='login')
@@ -708,18 +710,19 @@ class UsersListView(LoginRequiredMixin, generic.ListView):
 
 
 def route(request):
-
     context = {
-    "google_api_key": settings.GOOGLE_API_KEY,
-    "base_country": settings.BASE_COUNTRY}
+        "google_api_key": settings.GOOGLE_API_KEY,
+        "base_country": settings.BASE_COUNTRY,
+    }
     return render(request, 'main/route.html', context)
 
 
 '''
 Basic view for displaying a map 
 '''
-def map(request):
 
+
+def map(request):
     lat_a = request.GET.get("lat_a", None)
     long_a = request.GET.get("long_a", None)
     lat_b = request.GET.get("lat_b", None)
@@ -729,36 +732,34 @@ def map(request):
     lat_d = request.GET.get("lat_d", None)
     long_d = request.GET.get("long_d", None)
 
-
-    #only call API if all 4 addresses are added
+    # only call API if all 4 addresses are added
     if lat_a and lat_b and lat_c and lat_d:
         directions = Directions(
-            lat_a= lat_a,
+            lat_a=lat_a,
             long_a=long_a,
-            lat_b = lat_b,
+            lat_b=lat_b,
             long_b=long_b,
-            lat_c= lat_c,
+            lat_c=lat_c,
             long_c=long_c,
-            lat_d = lat_d,
-            long_d=long_d
-            )
+            lat_d=lat_d,
+            long_d=long_d,
+        )
     else:
         return redirect(reverse('main:route'))
 
     context = {
-    "google_api_key": settings.GOOGLE_API_KEY,
-    "base_country": settings.BASE_COUNTRY,
-    "lat_a": lat_a,
-    "long_a": long_a,
-    "lat_b": lat_b,
-    "long_b": long_b,
-    "lat_c": lat_c,
-    "long_c": long_c,
-    "lat_d": lat_d,
-    "long_d": long_d,
-    "origin": f'{lat_a}, {long_a}',
-    "destination": f'{lat_b}, {long_b}',
-    "directions": directions,
-
+        "google_api_key": settings.GOOGLE_API_KEY,
+        "base_country": settings.BASE_COUNTRY,
+        "lat_a": lat_a,
+        "long_a": long_a,
+        "lat_b": lat_b,
+        "long_b": long_b,
+        "lat_c": lat_c,
+        "long_c": long_c,
+        "lat_d": lat_d,
+        "long_d": long_d,
+        "origin": f'{lat_a}, {long_a}',
+        "destination": f'{lat_b}, {long_b}',
+        "directions": directions,
     }
     return render(request, 'main/map.html', context)
