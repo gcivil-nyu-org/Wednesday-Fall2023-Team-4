2ddd6ee18e7a818d24d8476172aac4c0731fa975
himanshu1196 <11.himanshu.96@gmail.com>
2023-11-23 01:17:13 -0500

    making redis point to aws cluster

--- roomierendezvous/settings.py
+++ roomierendezvous/settings.py
@@ -226,17 +226,15 @@ BASE_COUNTRY = "US"
 
 # chat config
 
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
-
-if "AWS_STORAGE_BUCKET_NAME" in os.environ:
-    CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
@@ -244,4 +242,14 @@ if "AWS_STORAGE_BUCKET_NAME" in os.environ:
         },
     },
 }
+
+if "AWS_STORAGE_BUCKET_NAME" in os.environ:
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            'CONFIG': {
+                "hosts": [('roomierendezvous-redis.ivffkn.ng.0001.use1.cache.amazonaws.com', 6379)],
+            },
+        },
+    }
 LOGIN_URL = 'rrapp:login'
