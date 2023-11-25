c68eaab9e262d6c11462b6464f2e0d952b9bee7d
himanshu1196 <11.himanshu.96@gmail.com>
2023-11-25 16:21:03 -0500

    fixed bug for public profile context

--- rrapp/views.py
+++ rrapp/views.py
@@ -642,7 +642,8 @@ class PublicProfileView(generic.DetailView):
     def get_context_data(self, **kwargs: Any):
         context_data = super().get_context_data(**kwargs)
         context_data["user_id"] = self.kwargs["pk"]
-        context_data["user"] = User.objects.get(id=self.kwargs["pk"])
+        context_data["user"] = self.request.user
+        context_data["other_user"] = User.objects.get(id=self.kwargs["pk"])
         context_data["path"] = self.request.path_info.__contains__("renter")
         context_data["inbox"] = get_inbox_count(
             User.objects.get(id=self.kwargs["pk"]).username
