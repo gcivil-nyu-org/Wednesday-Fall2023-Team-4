94ad2ef1b897a54501b529a342067124129ea283
himanshu1196 <11.himanshu.96@gmail.com>
2023-12-06 01:19:56 -0500

    -email and +verification status in public profile

--- rrapp/views.py
+++ rrapp/views.py
@@ -817,6 +817,8 @@ class PublicProfileView(generic.DetailView):
         'pets',
         'food_group',
         'rating',
+        'is_verified',
+        'verified_student',
     ]
 
     def get_context_data(self, **kwargs: Any):
