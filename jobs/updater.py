5666a831b7ef3a70793f2ed5399a7a46db6c7ac5
Zhiyuan Wei <zw3475@nyu.edu>
2023-12-09 21:25:30 -0500

    commit

--- jobs/updater.py
+++ jobs/updater.py
@@ -4,5 +4,5 @@ from .jobs import expire_student_status
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(expire_student_status, 'interval', seconds=5)
+    scheduler.add_job(expire_student_status, 'interval', seconds=86400)
     scheduler.start()
